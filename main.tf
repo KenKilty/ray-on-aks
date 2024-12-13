cb6c600c01df3c0cc2babc10167a1e4b3494e97e
Russell de Pina <rdepina@microsoft.com>
2024-12-13 15:20:06 -0600

    stash change

--- main.tf
+++ main.tf
@@ -17,11 +17,12 @@ resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
-  location            = azurerm_resource_group.rg.location
-  name                = random_pet.azurerm_kubernetes_cluster_name.id
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
-
+  location                         = azurerm_resource_group.rg.location
+  name                             = random_pet.azurerm_kubernetes_cluster_name.id
+  resource_group_name              = azurerm_resource_group.rg.name
+  dns_prefix                       = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
+  http_application_routing_enabled = true
+    
   identity {
     type = "SystemAssigned"
   }
